f5346fcd34811e065e0e5a13f83cbbc5f047bb49
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-08-19 01:12:15 -0400

    lesson One of the Stanford Course

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,15 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    // computed property that is tracking the display
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+        
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                //display.text = String(Double.pi)
+                displayValue = Double.pi
+            case "√":
+                //let operand = Double(display.text!)!
+                //display.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
